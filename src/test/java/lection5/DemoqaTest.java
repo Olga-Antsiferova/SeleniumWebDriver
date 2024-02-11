981fc9abfd5d87d9cf3bb60514af60204f4533b9
Olga-Antsiferova <atlasnsk@mail.ru>
2024-02-11 16:44:57 +0700

    Add files 11/02

--- src/test/java/lection5/DemoqaTest.java
+++ src/test/java/lection5/DemoqaTest.java
@@ -2,8 +2,7 @@ package lection5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoqaTest {
@@ -31,24 +30,46 @@ public class DemoqaTest {
     public void sendForm() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
 
         driver.get("https://demoqa.com/automation-practice-form");
+
+        WebElement selectState = driver.findElement(By.xpath("//*[@id=\"state\"]/div/div[1]/div[1]").);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",selectState);
+        selectState.click();
+        selectState.sendKeys(Keys.ARROW_DOWN, Keys.ENTER);
         driver.findElement(By.id("firstName")).sendKeys("Olga");
         Thread.sleep(2000);
         driver.findElement(By.id("lastName")).sendKeys("Antsiferova");
         Thread.sleep(2000);
         driver.findElement(By.id("userEmail")).sendKeys("atlasnsk@mail.ru");
         Thread.sleep(2000);
+        WebElement radioGender = driver.findElement(By.xpath("//*[@id='genterWrapper']/div[2]/div[2]/label"));
+        radioGender.click();
+        Thread.sleep(2000);
         driver.findElement(By.id("userNumber")).sendKeys("9231844527");
         Thread.sleep(2000);
-       // driver.findElement(By.cssSelector("[class = 'subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3']")).sendKeys("Chemistry");
-        //Thread.sleep(2000);
-        driver.findElement(By.id("currentAddress")).sendKeys("Novosibirsk");
+        driver.findElement(By.xpath("//*[@id=\"dateOfBirthInput\"]")).sendKeys(Keys.chord(Keys.SHIFT, Keys.CONTROL + "a"));
         Thread.sleep(2000);
-        driver.findElement(By.id("hobbies-checkbox-1")).click();
+        WebElement dateOfBirth = driver.findElement(By.xpath("//*[@id=\"dateOfBirthInput\"]"));
+        dateOfBirth.sendKeys("26 Nov 1983");
+        dateOfBirth.sendKeys(Keys.ENTER);
         Thread.sleep(2000);
-        driver.findElement(By.id("gender-radio-2")).click();
+        WebElement hobbiesCheckbox = driver.findElement(By.xpath("//*[@id=\"hobbiesWrapper\"]/div[2]/div[1]/label"));
+        hobbiesCheckbox.click();
         Thread.sleep(2000);
+        WebElement sendSubject = driver.findElement(By.xpath("//*[@id=\"subjectsContainer\"]/div"));
+        sendSubject.click();
+        sendSubject.sendKeys("Chemistry");
+        sendSubject.sendKeys(Keys.ENTER);
+        driver.findElement(By.cssSelector("[class='subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3']")).sendKeys("Chemistry");
+        driver.findElement(By.className("ui-menu-item")).click();
+        Thread.sleep(2000);
+
+
+
+
         driver.quit();
     }
 
